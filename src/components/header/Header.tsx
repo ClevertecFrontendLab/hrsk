0e0a83e9bd6b8050b32bb065c5aca40e3df5a4fe
hrsk <hrsk.yegor@gmail.com>
2025-04-12 09:10:33 +0300

    fix(layout): header

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,4 +1,4 @@
-import { Box, Flex, HStack, Text } from '@chakra-ui/react';
+import { Box, Flex, HStack, Text, useMediaQuery } from '@chakra-ui/react';
 
 import { Breadcrumbs } from '~/components/header/breadcrumbs/Breadcrumbs';
 import { HamburgerButton } from '~/components/header/hamburger_menu/HamburgerButton';
@@ -10,114 +10,117 @@ type Props = {
     onOpen: () => void;
 };
 
-export const Header = ({ onOpen }: Props) => (
-    <Flex
-        as='header'
-        data-test-id='header'
-        position='sticky'
-        zIndex={1000}
-        top={0}
-        backgroundColor='lime.500'
-        width='100%'
-        height='80px'
-        pl={{
-            sm: 4,
-            md: 5,
-            lg: 4,
-            xl: 4,
-        }}
-        pr={{ sm: 4, md: 5, lg: 14, xl: 14 }}
-        py={{ sm: 4, md: 4, lg: 4, xl: 4 }}
-        border='1px'
-        boxShadow='customShadow'
-        alignItems='center'
-    >
-        <HeaderLogo />
-        <Breadcrumbs />
-        <ProfileInfo />
-        <HStack
-            position='absolute'
-            right='68px'
-            maxWidth='203px'
-            maxHeight='24px'
-            display={{ sm: 'flex', md: 'flex', lg: 'none', xl: 'none' }}
+export const Header = ({ onOpen }: Props) => {
+    const [isVisible] = useMediaQuery('(min-width: 769px)');
+
+    return (
+        <Flex
+            as='header'
+            data-test-id='header'
+            position='sticky'
+            zIndex={1000}
+            top={0}
+            backgroundColor='lime.500'
+            width='100%'
+            height='80px'
+            pl={{
+                sm: 4,
+                md: 5,
+                lg: 4,
+                xl: 4,
+            }}
+            pr={{ sm: 4, md: 5, lg: 14, xl: 14 }}
+            py={{ sm: 4, md: 4, lg: 4, xl: 4 }}
+            boxShadow='customShadow'
             alignItems='center'
-            justifyContent='center'
-            px={{ sm: 2, md: 4 }}
         >
-            <Box
-                as='button'
-                display='flex'
-                alignItems='center'
-                maxWidth='57px'
+            <HeaderLogo />
+            <Breadcrumbs />
+            <ProfileInfo />
+            <HStack
+                position='absolute'
+                right='68px'
+                maxWidth='203px'
                 maxHeight='24px'
-                borderRadius='6px'
-            >
-                <SpriteIcon
-                    viewBox='16px 16px'
-                    boxSize='16px'
-                    spritePath='/spriteRightSide.svg'
-                    spriteId='bookmark'
-                />
-                <Text
-                    paddingLeft='8px'
-                    lineHeight={1.5}
-                    fontWeight={600}
-                    fontSize={{ sm: '12px', md: '12px' }}
-                    color='lime.600'
-                >
-                    185
-                </Text>
-            </Box>
-            <Box
-                as='button'
-                display='flex'
+                visibility={isVisible ? 'hidden' : 'visible'}
                 alignItems='center'
-                maxWidth='57px'
-                maxHeight='24px'
-                borderRadius='6px'
+                justifyContent='center'
+                px={{ sm: 2, md: 4 }}
             >
-                <SpriteIcon
-                    viewBox='16px'
-                    boxSize='16px'
-                    spritePath='/spriteRightSide.svg'
-                    spriteId='people'
-                />
-                <Text
-                    paddingLeft='8px'
-                    lineHeight={1.5}
-                    fontWeight={600}
-                    fontSize={{ sm: '12px', md: '12px' }}
-                    color='lime.600'
+                <Box
+                    as='button'
+                    display='flex'
+                    alignItems='center'
+                    maxWidth='57px'
+                    maxHeight='24px'
+                    borderRadius='6px'
                 >
-                    589
-                </Text>
-            </Box>
-            <Box
-                as='button'
-                display='flex'
-                alignItems='center'
-                maxWidth='57px'
-                maxHeight='24px'
-                borderRadius='6px'
-            >
-                <SpriteIcon
-                    viewBox='16px 16px'
-                    boxSize='16px'
-                    spritePath='/spriteRightSide.svg'
-                    spriteId='emoji'
-                />
-                <Text
-                    paddingLeft='8px'
-                    lineHeight={1.5}
-                    fontWeight={600}
-                    fontSize={{ sm: '12px', md: '12px' }}
-                    color='lime.600'
+                    <SpriteIcon
+                        viewBox='16px 16px'
+                        boxSize='16px'
+                        spritePath='/spriteRightSide.svg'
+                        spriteId='bookmark'
+                    />
+                    <Text
+                        paddingLeft='8px'
+                        lineHeight={1.5}
+                        fontWeight={600}
+                        fontSize={{ sm: '12px', md: '12px' }}
+                        color='lime.600'
+                    >
+                        185
+                    </Text>
+                </Box>
+                <Box
+                    as='button'
+                    display='flex'
+                    alignItems='center'
+                    maxWidth='57px'
+                    maxHeight='24px'
+                    borderRadius='6px'
                 >
-                    587
-                </Text>
-            </Box>
-        </HStack>
-        <HamburgerButton onOpen={onOpen} />
-    </Flex>
-);
+                    <SpriteIcon
+                        viewBox='16px'
+                        boxSize='16px'
+                        spritePath='/spriteRightSide.svg'
+                        spriteId='people'
+                    />
+                    <Text
+                        paddingLeft='8px'
+                        lineHeight={1.5}
+                        fontWeight={600}
+                        fontSize={{ sm: '12px', md: '12px' }}
+                        color='lime.600'
+                    >
+                        589
+                    </Text>
+                </Box>
+                <Box
+                    as='button'
+                    display='flex'
+                    alignItems='center'
+                    maxWidth='57px'
+                    maxHeight='24px'
+                    borderRadius='6px'
+                >
+                    <SpriteIcon
+                        viewBox='16px 16px'
+                        boxSize='16px'
+                        spritePath='/spriteRightSide.svg'
+                        spriteId='emoji'
+                    />
+                    <Text
+                        paddingLeft='8px'
+                        lineHeight={1.5}
+                        fontWeight={600}
+                        fontSize={{ sm: '12px', md: '12px' }}
+                        color='lime.600'
+                    >
+                        587
+                    </Text>
+                </Box>
+            </HStack>
+            <HamburgerButton onOpen={onOpen} />
+        </Flex>
+    );
+};
--- src/components/header/header_logo/HeaderLogo.tsx
+++ src/components/header/header_logo/HeaderLogo.tsx
@@ -1,17 +1,21 @@
-import { Box, Image } from '@chakra-ui/react';
+import { Box, Image, useMediaQuery } from '@chakra-ui/react';
 
 import logo_text from '~/assets/header/logo_text.svg';
 import pot from '~/assets/header/pot.svg';
 
-export const HeaderLogo = () => (
-    <Box display='flex' alignItems='center' maxWidth='135px' maxHeight='32px'>
-        <Image boxSize='32px' src={pot} alt='pot' objectFit='cover' />
-        <Image
-            width='96px'
-            src={logo_text}
-            alt='logo text - yee daa'
-            objectFit='cover'
-            display={{ sm: 'none', md: 'flex' }}
-        />
-    </Box>
-);
+export const HeaderLogo = () => {
+    const [isVisible] = useMediaQuery('(min-width: 361px)');
+
+    return (
+        <Box display='flex' alignItems='center' maxWidth='135px' maxHeight='32px'>
+            <Image boxSize='32px' src={pot} alt='pot' objectFit='cover' />
+            <Image
+                width='96px'
+                src={logo_text}
+                alt='logo text - yee daa'
+                objectFit='cover'
+                visibility={isVisible ? 'visible' : 'hidden'}
+            />
+        </Box>
+    );
+};
